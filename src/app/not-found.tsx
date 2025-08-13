cde15ee91d274d9028a33d6cc6e0e264c88b193e
William <help@rapidhomeaid.com>
2025-08-13 12:31:53 -0400

    Fix: rewrite not-found.tsx with UTF-8 encoding

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,8 +1,8 @@
-export default function NotFound() {
+﻿export default function NotFound() {
   return (
     <main className="h-screen flex flex-col items-center justify-center">
       <h1 className="text-4xl font-bold">Page Not Found</h1>
-      <p className="mt-2">Sorry, we can�t find that page.</p>
+      <p className="mt-2 text-slate-700">Sorry, we can’t find that page.</p>
     </main>
   );
 }
